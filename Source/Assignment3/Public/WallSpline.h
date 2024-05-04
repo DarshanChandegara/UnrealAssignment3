cb6bcd4a2594ba3ef5c2db97348420170fa4dcd4
Darshan Chandegara <darshan06ppsv@gmail.com>
2024-05-04 10:08:44 +0530

    Completed Assignment 3

---
+++ Source/Assignment3/Public/WallSpline.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include <Components/SplineComponent.h>
+#include <Components/SplineMeshComponent.h>
+#include "WallSpline.generated.h"
+
+UCLASS()
+class ASSIGNMENT3_API AWallSpline : public APawn
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	USplineComponent* spline; 
+
+	UPROPERTY(VisibleDefaultsOnly)
+	USceneComponent* scene;
+
+	UPROPERTY()
+	TArray<USplineMeshComponent*> WallArray;
+	
+
+public:
+	AWallSpline();
+
+	UPROPERTY()
+	TArray<FVector> points;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite) 
+	UStaticMesh* staticMesh;
+
+	UPROPERTY(EditAnywhere , BlueprintReadWrite)
+	UMaterial* material;
+
+	UFUNCTION(BlueprintCallable) 
+	void GenerateWalls();
+	//void GenerateWalls();
+	void generatePoints(FVector& point);
+
+
+	//void generatePoints();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	void DestroyWalls();
+
+	void ClearPoints();
+
+	void AddPoint(FVector point);
+
+	void RemoveLastPoint();
+
+	int32 GetSplinePoints();
+};
